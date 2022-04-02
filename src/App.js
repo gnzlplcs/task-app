d76042394f3eddec9e797e0bf51e11bc0caadf5e
gnzlplcs <palaciosgm@outlook.com>
2022-04-02 18:49:47 -0500

    :pencil2: fix typo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import freeCodeCampLogo from "./imagenes/logo-freecodecamp.png";
+import freeCodeCampLogo from "./imagenes/freecodecamp-logo.png";
 
 function App() {
   return (
