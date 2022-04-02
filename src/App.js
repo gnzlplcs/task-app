8321c941dbf56597db484b010421814a1966aa9b
gnzlplcs <palaciosgm@outlook.com>
2022-04-02 18:46:26 -0500

    :tada: init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import freeCodeCampLogo from "./imagenes/logo-freecodecamp.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="aplicacion-tareas">
+      <div className="freecodecamp-logo-contenedor">
+        <img
+          src={freeCodeCampLogo}
+          alt="Logo de freeCodeCamp"
+          className="freecodecamp-logo"
+        />
+      </div>
     </div>
   );
 }
