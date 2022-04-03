6affc469ec3bf48754261578c2f726dae97e6ee0
gnzlplcs <palaciosgm@outlook.com>
2022-04-02 19:57:29 -0500

    :art: main container styled

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ function App() {
   return (
     <div className="aplicacion-tareas">
       <Logo />
+      <div className="tareas-lista-principal">
+        <h1>Mis Tareas</h1>
+      </div>
+
     </div>
   );
 }
