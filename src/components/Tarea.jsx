6ef5cedcba0391f7cf524d643baeef1d0cfa2afa
gnzlplcs <palaciosgm@outlook.com>
2022-04-02 21:48:08 -0500

    :sparkles: add react-icons
    add delete task icon

--- src/components/Tarea.jsx
+++ src/components/Tarea.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import '../styles/Tarea.css';
+import { AiOutlineCloseCircle } from 'react-icons/ai';
 
 const Tarea = ({ texto }) => {
   return (
     <div className='tarea-contenedor'>
       <div className="tarea-texto">{texto}</div>
-      <div className="tarea-icono">
-        Eliminar
+      <div className="tarea-contenedor-iconos">
+        <AiOutlineCloseCircle className="tarea-icono" />
       </div>
     </div>
   )
