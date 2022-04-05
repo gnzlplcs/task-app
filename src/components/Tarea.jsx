35d0b3fb9875313ec8e1a873c14a251edb2d8945
gnzlplcs <palaciosgm@outlook.com>
2022-04-05 15:16:44 -0500

    🛬 passing prop

--- src/components/Tarea.jsx
+++ src/components/Tarea.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
-import '../styles/Tarea.css';
-import { AiOutlineCloseCircle } from 'react-icons/ai';
+import React from "react";
+import "../styles/Tarea.css";
+import { AiOutlineCloseCircle } from "react-icons/ai";
 
-const Tarea = ({ texto }) => {
+const Tarea = ({ texto, completada }) => {
   return (
-    <div className='tarea-contenedor'>
+    <div
+      className={
+        completada ? "tarea-contenedor completada" : "tarea-contenedor"
+      }
+    >
       <div className="tarea-texto">{texto}</div>
       <div className="tarea-contenedor-iconos">
         <AiOutlineCloseCircle className="tarea-icono" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Tarea
+export default Tarea;
